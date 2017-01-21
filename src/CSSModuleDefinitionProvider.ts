aade870fefb0d4e61c11dc46b2edaadd4a35ebbc
clinyong <clinyong@gmail.com>
2017-01-21 10:41:25 +0800

    go to definition

---
+++ src/CSSModuleDefinitionProvider.ts
@@ -0,0 +1,75 @@
+import { DefinitionProvider, TextDocument, Position, CancellationToken, Location, Uri } from "vscode";
+import { getCurrentLine, findImportPath, isCSSLikeFile } from "./utils";
+import * as path from "path";
+import * as fs from "fs";
+
+function getTextWithinString(text: string) {
+    const start = text.indexOf('"');
+    const end = text.lastIndexOf('"');
+
+    return text.slice(start + 1, end);
+}
+
+function getVar(line: string, position: Position): string {
+    const headText = line.slice(0, position.character)
+    const tailText = line.slice(position.character)
+
+    const startBySpace = headText.lastIndexOf(" ");
+    const startByBracket = headText.lastIndexOf("{");
+
+    const endBySpace = tailText.indexOf(" ")
+    const endByBracket = tailText.indexOf("}")
+
+    if (startBySpace === startByBracket) {
+        return "";
+    }
+
+    if (startBySpace > startByBracket) {
+        return `${headText.slice(startBySpace + 1)}${tailText.slice(0, endBySpace)}`
+    } else {
+        return `${headText.slice(startByBracket + 1)}${tailText.slice(0, endByBracket)}`
+    }
+}
+
+function getPosition(filePath: string, keyWord: string): Position {
+    const content = fs.readFileSync(filePath, { encoding: "utf8" });
+    const lines = content.split("\n")
+
+    let index = -1
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].indexOf(keyWord) !== -1) {
+            index = i
+            break
+        }
+    }
+
+    if (index === -1) {
+        return null
+    } else {
+        return new Position(index, 0)
+    }
+}
+
+export class CSSModuleDefinitionProvider implements DefinitionProvider {
+    public provideDefinition(document: TextDocument, position: Position, token: CancellationToken): Thenable<Location> {
+        const currentLine = getCurrentLine(document, position);
+        const word = getVar(currentLine, position);
+        const currentDir = path.dirname(document.uri.fsPath);
+
+        const [obj, field] = word.split(".")
+        const importPath = findImportPath(document.getText(), obj, currentDir)
+        if (!isCSSLikeFile(importPath)) {
+            return Promise.resolve(null);
+        }
+
+        const targetPosition = getPosition(importPath, field)
+
+        if (targetPosition === null) {
+            return Promise.resolve(null);
+        } else {
+            return Promise.resolve(new Location(Uri.file(importPath), targetPosition));
+        }
+    }
+}
+
+export default CSSModuleDefinitionProvider;
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
 'use strict';
 import { languages, ExtensionContext, DocumentFilter } from 'vscode';
 import { CSSModuleCompletionProvider } from "./CSSModuleCompletionProvider";
+import { CSSModuleDefinitionProvider } from "./CSSModuleDefinitionProvider";
 
 export function activate(context: ExtensionContext) {
     const mode: DocumentFilter = { language: 'plaintext', scheme: 'file' };
     const trigger = "\.";
     context.subscriptions.push(
         languages.registerCompletionItemProvider(mode, new CSSModuleCompletionProvider(), trigger)
+    );
+    context.subscriptions.push(
+        languages.registerDefinitionProvider(mode, new CSSModuleDefinitionProvider())
     );
 }
 
