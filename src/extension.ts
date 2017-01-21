7c779fa695b2a55f910b4dd521d098c6110fe0c0
clinyong <clinyong@gmail.com>
2017-01-21 11:24:27 +0800

    fix trigger event

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,10 @@ import { CSSModuleCompletionProvider } from "./CSSModuleCompletionProvider";
 import { CSSModuleDefinitionProvider } from "./CSSModuleDefinitionProvider";
 
 export function activate(context: ExtensionContext) {
-    const mode: DocumentFilter = { language: 'plaintext', scheme: 'file' };
+    const mode: DocumentFilter = [
+        { language: 'typescriptreact', scheme: 'file' },
+        { language: 'javascriptreact', scheme: 'file' }
+    ];
     const trigger = "\.";
     context.subscriptions.push(
         languages.registerCompletionItemProvider(mode, new CSSModuleCompletionProvider(), trigger)
