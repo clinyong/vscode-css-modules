8d02901a44041dd87212d3e8cbf53c7d5a7048d9
clinyong <clinyong@gmail.com>
2017-02-02 18:22:31 +0800

    add completion test

---
+++ test/CSSModuleCompletionProvider.test.ts
@@ -0,0 +1,22 @@
+import * as assert from "assert";
+import * as vscode from "vscode";
+import * as path from "path";
+import { CSSModuleCompletionProvider } from "../src/CSSModuleCompletionProvider";
+
+const rootPath = path.join(__dirname, "../..");
+const tsxFile = path.join(rootPath, "./test/fixtures/sample.tsx");
+const uri = vscode.Uri.file(tsxFile);
+
+test("test completion items length", done => {
+    return vscode.workspace.openTextDocument(uri).then(text => {
+        const provider = new CSSModuleCompletionProvider();
+        const position = new vscode.Position(1, 19);
+        return provider.provideCompletionItems(text, position).then(items => {
+            assert.equal(4, items.length);
+            done();
+        });
+    }, err => {
+        assert.ok(false, `error in OpenTextDocument ${err}`);
+        done(err);
+    });
+});
