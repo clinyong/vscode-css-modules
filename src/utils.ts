abedb7583003a28fdb6ddea7d49764541f6aef9b
nataxo <38152408+nataxo@users.noreply.github.com>
2020-02-09 00:00:28 +0300

    add stylus support

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@ export function getCurrentLine(document: TextDocument, position: Position): stri
 }
 
 export function genImportRegExp(key: string ): RegExp {
-    const file = "(.+\\.\\S{1,2}ss)";
+    const file = "(.+\\.(\\S{1,2}ss|styl))";
     const fromOrRequire = "(?:from\\s+|=\\s+require(?:<any>)?\\()";
     const requireEndOptional = "\\)?";
     const pattern = `${key}\\s+${fromOrRequire}["']${file}["']${requireEndOptional}`;
